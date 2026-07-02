421751145e03734946c2e1cbfba94510055c0236
AshuJavaDev <nishukr5454@gmail.com>
2026-07-02 11:32:59 +0530

    React Router - Home and Notes pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,21 @@
-import {useState, useEffect} from 'react'
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from './Home.jsx'
+import Notes from './Notes.jsx'
 
-function StudyRoom(props) {
-  
-  useEffect( () =>  {
-    console.log(props.name + " loaded!")
-  }, [])
-  
+function App() {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>{props.count} students studying now</p>
-      {props.count>=30 ? <p>Room Full</p> : <button onClick = {props.onJoin}>Join Room</button>}
+      <nav>
+        <Link to="/">Home</Link>  |
+        <Link to ="/notes">Notes</Link>
+      </nav>
+
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/notes" element = {<Notes />} />
+        </Routes>
     </div>
   )
 }
 
-
-
-function App()  {
- const [rooms, setRooms] = useState([
-  {name: "UPSC Room", count: 25},
-  {name: "NEET Room", count: 25},
-  {name: "JEE Room", count: 25},
-  {name: "CA Room", count: 25}
- ])
-
- const[nickname, setNickname] = useState("")
- const[isSubmitted, setIsSubmitted] = useState(false)
- const[exam, setExam] = useState("") 
- const[topic, setTopic] = useState("")
-
-
-function increaseCount(index)  {
-  const newRooms = [...rooms]
-  newRooms[index].count = newRooms[index].count + 1
-  setRooms(newRooms)
-}
-
-return (
-  <div>
-    <h1>PadhakuGang</h1>
-    <p>Total students: {rooms.reduce((sum,room) => sum + room.count, 0)}</p>
-    {rooms.map((room,index) => (
-      <StudyRoom
-        key = {room.name}
-        name ={room.name}
-        count = {room.count}
-        onJoin = {() => increaseCount(index)}
-        />      
-    ))}
-
-    <input type = "text"      
-    placeholder = "Enter your nickname"    
-    value = {nickname} 
-    onChange = {(e) => setNickname(e.target.value)} 
-    />
-
-    {isSubmitted && <p>Your nickname: {nickname}</p> }
-
-
-
-<input type = "text"
- placeholder = "Preparing for? (UPSC, NEET....)"
- value = {exam}
- onChange = {(e) => setExam(e.target.value)}
- />
- {isSubmitted && <p>Preparing for: {exam}</p>}
-
-  <input type = "text"
-  placeholder = "Today's topic?"
-  value = {topic}
-  onChange = {(e) => setTopic(e.target.value)}
-  />
-  {isSubmitted && <p>Today's topic: {topic}</p>}
-
-  <button onClick = {() => setIsSubmitted(true)}>Join</button>
-{isSubmitted ? <p>Welcome, {nickname} | {exam} |  {topic}</p> : null}
-
-
-  </div>
-  )
-}
-
 export default App
-
---
+++ src/Notes.jsx
@@ -0,0 +1,11 @@
+function Notes() {
+    return (
+        <div>
+            <h2>Notes </h2>
+            <p>Important notes will appear here!</p>
+        </div>
+    )
+}
+
+
+export default Notes
